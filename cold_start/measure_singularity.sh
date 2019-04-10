ec5a997af112a7048adc2154e77ea661fb8130d2
Zhuozhao Li <zhuozhl@clemson.edu>
2019-04-10 02:02:20 +0000

    cold start for singularity on theta

--- cold_start/measure_singularity.sh
+++ cold_start/measure_singularity.sh
@@ -2,5 +2,5 @@
 
 for i in `seq 1 1 100`
 do
-    time singularity --image="ZZ_TO_FILL" -c "import parsl; print(parsl.__version__, parsl.__file__)" 
+    time singularity exec /home/zzli/sing-run.simg python3 -c "import parsl; print(parsl.__version__, parsl.__file__)" 
 done
